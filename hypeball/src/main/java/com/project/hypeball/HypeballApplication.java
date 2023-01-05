7347b3576931d0a3d9453e54337630be5a30a29c
ddongpuri <fnffnfkffk1112@gmail.com>
2023-01-05 20:01:35 +0900

    지도 기본 화면 구현

--- hypeball/src/main/java/com/project/hypeball/HypeballApplication.java
+++ hypeball/src/main/java/com/project/hypeball/HypeballApplication.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HypeballApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HypeballApplication.class, args);
-
-
 	}
 }
