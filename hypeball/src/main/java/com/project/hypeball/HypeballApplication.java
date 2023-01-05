ff16aff06b6cc5d27e08ae5bcf1c751cb48f4e18
ddongpuri <fnffnfkffk1112@gmail.com>
2023-01-05 15:42:54 +0900

    initialized file

---
+++ hypeball/src/main/java/com/project/hypeball/HypeballApplication.java
@@ -0,0 +1,13 @@
+package com.project.hypeball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypeballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypeballApplication.class, args);
+	}
+
+}
---
+++ hypeball/src/test/java/com/project/hypeball/HypeballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hypeball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypeballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
